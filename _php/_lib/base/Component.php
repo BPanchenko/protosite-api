5e7a5c4b03c103e4ff6d4b4ddc79c06509179048
BPanchenko <oberon@list.ru>
2015-02-20 00:45:02 +0300

    the beginning of the synchronization components

--- _php/_lib/base/Component.php
+++ _php/_lib/base/Component.php
@@ -28,20 +28,37 @@ namespace base;
 		
 		public function fetch($options) {
 			
-			$_default_options = array(
+			if(is_string($this->_table)) $this->_initTable();
+			
+			// 
+			$_fields = array();
+			if(isset($options['excluded_fields'])) {
+				$options['excluded_fields'] = str2array($options['excluded_fields']);
+			}
+			
+			$_opt = array(
 				'where' => (array)$options['where'],
-				'fields' => array(),
+				'fields' => is_array($options['fields']) ? $options['fields'] : '*',
+				'order' => '`id` DESC',
 				'count' => 20,
 				'offset' => 0
 			);
 			$options = array_merge($_default_options, $options);
 			
-			if(is_string($this->_table))
-				$this->_initTable();
-				
-			// var_dump($this->_parent);
+			// 
+			if(isset($options['fields']))
+				$options['fields'] = str2array($options['fields']);
+			else
+				$options['fields'] = $this->_table->columns();
 			
-			return $this;
+			// 
+			$res = $this->_table->reset()
+								->select($options['fields'])
+								->limit($options['count'])
+								->offset($options['offset'])
+								->fetchAll(\PDO::FETCH_ASSOC);
+			
+			return $this->set($res);
 		}
 		
 		protected function _initTable() {
