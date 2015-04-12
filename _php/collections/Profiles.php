fccd0a24c0921a6ae9e440abca3a2a870b803dd8
BPanchenko <oberon@list.ru>
2015-04-12 23:16:24 +0300

    Revert "add system components"
    This reverts commit bb238c64df5e319535dbd1a16b2fa054b8be07c9.

--- _php/collections/Profiles.php
+++
@@ -1,6 +0,0 @@
-<?php
-	class Profiles extends \base\Collection {
-		public static $classModel = Profile;
-		protected $_table = 'mysql:bp_instastat.profiles';
-	}
-?>
--- _php/models/Comment.php
+++
@@ -1,5 +0,0 @@
-<?php
-	class Comment extends \base\Model {
-		public static $idAttribute = 'comment_id';
-	}
-?>
