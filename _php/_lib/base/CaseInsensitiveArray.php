510f02ea66f6a5d1a7ffd7dac6ff68f958fe74af
BPanchenko <oberon@list.ru>
2015-10-04 04:48:16 +0300

    refactoring

--- _php/_lib/base/CaseInsensitiveArray.php
+++ _php/_lib/base/CaseInsensitiveArray.php
@@ -2,53 +2,53 @@
 namespace base;
 
 	class CaseInsensitiveArray implements \ArrayAccess, \Countable, \Iterator {
-		private $container = array();
+		private $_container = array();
 	
 		public function offsetSet($offset, $value) {
 			if (is_null($offset)) {
-				$this->container[] = $value;
+				$this->_container[] = $value;
 			} else {
-				$index = array_search(strtolower($offset), array_keys(array_change_key_case($this->container, CASE_LOWER)));
-				if (!($index === false)) {
-					$keys = array_keys($this->container);
-					unset($this->container[$keys[$index]]);
+				$index = array_search(strtolower($offset), array_keys(array_change_key_case($this->_container, CASE_LOWER)));
+				if ($index !== false) {
+					$keys = array_keys($this->_container);
+					unset($this->_container[$keys[$index]]);
 				}
-				$this->container[$offset] = $value;
+				$this->_container[$offset] = $value;
 			}
 		}
 	
 		public function offsetExists($offset) {
-			return array_key_exists(strtolower($offset), array_change_key_case($this->container, CASE_LOWER));
+			return array_key_exists(strtolower($offset), array_change_key_case($this->_container, CASE_LOWER));
 		}
 	
 		public function offsetUnset($offset) {
-			unset($this->container[$offset]);
+			unset($this->_container[$offset]);
 		}
 	
 		public function offsetGet($offset) {
-			$index = array_search(strtolower($offset), array_keys(array_change_key_case($this->container, CASE_LOWER)));
+			$index = array_search(strtolower($offset), array_keys(array_change_key_case($this->_container, CASE_LOWER)));
 			if ($index === false) {
 				return null;
 			}
 	
-			$values = array_values($this->container);
+			$values = array_values($this->_container);
 			return $values[$index];
 		}
 	
 		public function count() {
-			return count($this->container);
+			return count($this->_container);
 		}
 	
 		public function current() {
-			return current($this->container);
+			return current($this->_container);
 		}
 	
 		public function next() {
-			return next($this->container);
+			return next($this->_container);
 		}
 	
 		public function key() {
-			return key($this->container);
+			return key($this->_container);
 		}
 	
 		public function valid() {
@@ -56,11 +56,11 @@ namespace base;
 		}
 	
 		public function rewind() {
-			reset($this->container);
+			reset($this->_container);
 		}
 		
 		public function push($value) {
-			array_push($this->container, $value);
+			array_push($this->_container, $value);
 		}
 	}
 ?>
