1bae9b6c9d7577da9a5ccdc000748dc693364710
BPanchenko <oberon@list.ru>
2015-10-25 18:45:06 +0300

    optimization: the table model this is the collection table

--- _php/_lib/base/Component.php
+++ _php/_lib/base/Component.php
@@ -2,30 +2,31 @@
 namespace base;
 
 	abstract class Component {
-
-		protected $_childrens = array();
+		
+		protected $_children = array();
 		protected $_default_fetch_options = array(
 			'fields' => array(),
-			'excluded_fields' => array('is_del'),
+            'excluded_fields' => array('is_del'),
+            'where' => 'is_del:0',
 			'order' => NULL,
 			'count' => 100
 		);
 		protected $_parent;
 		protected $_table = '';
 		protected $_tables = array();
-
-
+		
+		
 		function __construct($data = array(), $parent = NULL) {
+
 			if($parent instanceof \base\Component)
 				$this->attachTo($parent);
-			
+
             if(is_string($this->_table) && $this->_table)
                 $this->_table = self::initTable($this->_table);
 
             if(is_array($this->_tables) && count($this->_tables))
                 foreach($this->_tables as $_tb_name=>$_tb_dns) {
-                    if($_tb_dns instanceof \PDO)
-                        continue;
+                    if($_tb_dns instanceof \PDO) continue;
 
                     if(strpos($_tb_dns, '{model_id}') !== false && !$this->isNew()) {
                         $_tb_dns = str_replace('{model_id}', $this->id, $_tb_dns);
@@ -34,55 +35,60 @@ namespace base;
                         $this->_tables[$_tb_name] = self::initTable($_tb_dns);
                 }
 		}
-
+		
 		public function trigger($name) {
-
+			
 		}
-
-		public function attach($parent_object) {
-			if($parent instanceof \Component)
-				array_push($_childrens, $parent_object);
+		
+		public function attach($child) {
+			if($child instanceof \Component)
+				array_push($this->_children, $parent_object);
 			return $this;
 		}
-
+		
 		public function attachTo($parent_object) {
-			if($parent_object instanceof \base\Component) {
+
+            if($parent_object instanceof \base\Component) {
 				$this->_parent = $parent_object;
 				$parent_object->attach($this);
 			}
-			if($this instanceof \base\Model && $parent_object instanceof \base\Collection)
-				$this->collection = $parent_object;
 
+			if($this instanceof \base\Model && $parent_object instanceof \base\Collection) {
+                $this->collection = $parent_object;
+                if($this->collection->_table instanceof \PDO)
+                    $this->_table = $this->collection->_table;
+            }
+			
 			return $this;
 		}
-
+		
 		public function isValid() {
 			return true;
 		}
-
-
+		
+		
 		/* Synchronization of the component with database support
 		 ========================================================================== */
-
-        public function fetch($options) {
+		
+		public function fetch($options) {
             $options = $this->_prepareFetchOptions($options);
 
-            //
-            $this->_table->reset()
-                ->select($options['fields'])
-                ->where($options['where']);
+			// 
+			$this->_table->reset()
+                         ->select($options['fields'])
+						 ->where($options['where']);
 
-            if($this instanceof \base\Model)
-                $this->_table->limit(1);
-            else
-                $this->_table->limit($options['count'])
-                    ->offset($options['offset'])
-                    ->order($options['order']);
-
-            $res = $this->_table->fetchAll(\PDO::FETCH_ASSOC);
-
-            return $this->set( $this instanceof \base\Model ? $res[0] : $res);
-        }
+			if($this instanceof \base\Model)
+				$this->_table->limit(1);
+			else
+				$this->_table->limit($options['count'])
+						 ->offset($options['offset'])
+						 ->order($options['order']);
+			
+			$res = $this->_table->fetchAll(\PDO::FETCH_ASSOC);
+			
+			return $this->set( $this instanceof \base\Model ? $res[0] : $res);
+		}
 
         protected function _prepareFetchOptions($options) {
 
@@ -174,59 +180,59 @@ namespace base;
             return $options;
         }
 
-        /**
+		/**
 		 * @method initTable - helper method component framework.
 		 * The initialization of an object of class Table.
-		 * @param $table -
+		 * @param $table - 
 		 */
 		public static function initTable($table) {
-
+			
 			if($table instanceof \DB\Schema)
 				return $table;
-
+				
 			if(empty($table))
 				throw new \SystemException('EmptyTableName');
-
+			
 			if(strpos($table, 'sqlite:') === 0)
 				$table = new \DB\SQLite\Table($table);
-
+				
 			if(strpos($table, 'mysql:') === 0)
 				$table = new \DB\MySql\Table($table);
-
+			
 			if(!($table instanceof \DB\Schema))
 				throw new \SystemException('FailInitComponentTable');
-
+			
 			return $table;
 		}
-
-
+		
+		
 		/* Insert or update a component in the database
 		 ========================================================================== */
-
+		
 		public function save($data) {
 			if($data instanceof \stdClass)
 				$data = json_decode(json_encode($data), true);
-
+			
 			if(is_array($data) && count($data))
 				$this->set($this->parse($data));
-
+			
 			// save models of collection
 			if($this instanceof \base\Collection) {
 				foreach($this as $model)
 					$model->save();
 				return $this;
 			}
-
+			
 			// save model
 			$this->_table->save($this->toArray());
-
+			
 			return $this;
 		}
-
-
+		
+		
 		/* Prepare api response
 		 ========================================================================== */
-
+		
 		public function prepareResponse($Response) {
 			$Response->set('data', $this->toArray());
 			return $this;
