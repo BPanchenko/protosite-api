15d0be966ab4dbe3526449c382087fa91fd710b2
BPanchenko <oberon@list.ru>
2016-05-05 23:25:59 +0300

    refactoring

--- _php/core.php
+++ _php/core.php
@@ -1,15 +1,8 @@
 <?
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 0");
-header("Cache-Control: public, max-age=0, must-revalidate"); 
-header("Expires: " . gmdate('D, d M Y H:i:s', time() - 1800) . " GMT");
-header("Content-Type: application/json; charset=utf-8");
-
 define('PHP_DIR', dirname(__FILE__));
 define('PHP_LIB_DIR', dirname(__FILE__) . "/_lib");
 
+require_once PHP_DIR . "/headers.php";
 require_once PHP_DIR . "/_settings.php";
 require_once PHP_DIR . "/_lib/functions.php";
 
