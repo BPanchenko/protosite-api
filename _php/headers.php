b47c79f95aa83ea309a602bbac876def960e955c
BPanchenko <oberon@list.ru>
2016-05-05 23:21:24 +0300

    supporting header 'Access-Control-Allow-Headers'

--- _php/headers.php
+++ _php/headers.php
@@ -1,7 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://yourdomain.com");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATH, DELETE, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, toquery");
 header("Access-Control-Max-Age: 0");
 header("Cache-Control: public, max-age=0, must-revalidate");
 header("Expires: " . gmdate('D, d М Y H:i:s', time() + 1800) . " GMT");
