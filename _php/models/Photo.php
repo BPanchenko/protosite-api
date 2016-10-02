538422550c9b557e19f460298908d5e4d6bf95fa
BPanchenko <oberon@list.ru>
2016-10-02 21:18:58 +0300

    define source path to photo

--- _php/models/Photo.php
+++ _php/models/Photo.php
@@ -3,4 +3,9 @@
 class Photo extends \base\Model {
     public static $idAttribute = 'photo_id';
     protected $_table = "mysql:photos";
+
+    public function parse($data = array()){
+        $data['src'] = PHOTOS_PATH . '/' . $data['folder'] . '/' . $data['filename'] . '.' . $data['extension'];
+        return $data;
+    }
 }
