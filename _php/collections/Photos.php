7995f9b0c1e661a1daa2aa0eab3046a92b7541a8
BPanchenko <oberon@list.ru>
2016-10-03 23:55:34 +0300

    upload photo to temp dir

--- _php/collections/Photos.php
+++ _php/collections/Photos.php
@@ -3,4 +3,14 @@
 class Photos extends base\Collection {
     public static $classModel = Photo;
     protected $_table = "mysql:photos";
+
+    /* public API
+     ========================================================================== */
+
+    public function POST__upload(){
+        $file = upload_file($_FILES['file'], PHOTOS_UPLOAD_DIR, 'images');
+        $file['folder'] = 'temp';
+        $Photo = $this->create($file)->save();
+        return $Photo->toArray();
+    }
 }
