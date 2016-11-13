6f4efe801680054d95df87ec87c031398df390ca
BPanchenko <oberon@list.ru>
2016-11-13 22:39:05 +0300

    sorting parameters by key

--- _php/lib/http/RequestParametersModel.php
+++ _php/lib/http/RequestParametersModel.php
@@ -1,31 +1,37 @@
 <?php
 namespace http;
-	class RequestParametersModel extends \base\Model {
-			
-		protected $_defaults = array(
-			'fields' => NULL,
-			'excluded_fields' => array('is_del'),
-			'count' => FETCH_DEFAULT_COUNT,
-			'offset' => FETCH_DEFAULT_OFFSET
-		);
-		
-		/****/
-		public function parse(array $data = array()) {
-			
-			if(isset($data['fields']))
-				$data['fields'] = str2array($data['fields']);
-				
-			if(isset($data['excluded_fields']))
-				$data['excluded_fields'] = str2array($data['excluded_fields']);
-			
-			if(isset($data['order']) && strpos($data['order'], '`') === false) {
-				if(strpos($data['order'], '-') === 0)
-					$data['order'] = '`' . str_replace('-','',$data['order']) . '` DESC';
-				else
-					$data['order'] = '`' . str_replace('-','',$data['order']) . '` ASC';
-			}
-			
-			return $data;
+class RequestParametersModel extends \base\Model {
+
+	protected $_defaults = array(
+		'fields' => NULL,
+		'excluded_fields' => array('is_del'),
+		'count' => FETCH_DEFAULT_COUNT,
+		'offset' => FETCH_DEFAULT_OFFSET
+	);
+
+	/****/
+	public function parse(array $data = array()) {
+
+		if(isset($data['fields'])) {
+			$data['fields'] = str2array($data['fields']);
+			sort($data['fields']);
 		}
+
+		if(isset($data['excluded_fields']))
+			$data['excluded_fields'] = str2array($data['excluded_fields']);
+
+		if(isset($data['order']) && strpos($data['order'], '`') === false) {
+			if(strpos($data['order'], '-') === 0)
+				$data['order'] = '`' . str_replace('-','',$data['order']) . '` DESC';
+			else
+				$data['order'] = '`' . str_replace('-','',$data['order']) . '` ASC';
+		}
+
+		// сортировка в порядке возрастания выполняется
+		// для совпадения постоянства $cache_filename при изменении места указания параметров
+		ksort($data);
+
+		return $data;
 	}
+}
 ?>
