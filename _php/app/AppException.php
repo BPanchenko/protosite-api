e7a993ca2c7aa833736e39542261d45f028fb02d
BPanchenko <oberon@list.ru>
2016-11-19 11:19:42 +0300

    refactoring app exceptions

--- _php/app/AppException.php
+++ _php/app/AppException.php
@@ -1,122 +1,88 @@
 <?php
 	class AppException extends Exception {
-		public $type;
-		public $data;
-		public $error;
+		private $_type;
+		private $_data;
+		private $error;
 		
 		private $_hash = array(
+			'EmailExists' => array(
+				'code' => 409,
+				'message' => "Email is already registered"
+			),
+            'MethodNotAllowed' => array(
+                'code' => 405,
+                'message' => "Method is not supported"
+            ),
+			'ManyRequestCheckpoints' => array(
+				'code' => 405,
+				'message' => "Many checkpoints in the request"
+			),
+			'WrongEmail' => array(
+				'code' => 400,
+				'message' => "Invalid email"
+			),
+			'WrongFileType' => array(
+				'code' => 400,
+				'message' => "Not registered file type"
+			),
+			'WrongPassword' => array(
+				'code' => 400,
+				'message' => "Invalid password"
+			),
+			'Unauthorized' => array(
+				'code' => 401,
+				'message' => "Access denied"
+			),
 			'UnknownError' => array(
 				'code' => 400,
-				'error_message' => array(
-					'en' => "Unknown Error",
-					'ru' => "Неизвестная ошибка"
-				)
-			),
-			'AccessDenied' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "Access denied",
-					'ru' => "Доступ запрещен"
-				)
-			),
-			'AccessTokenInvalid' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "Invalid a access token",
-					'ru' => "Недействительный ключ доступа"
-				)
-			),
-			'AccessTokenRequired' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "Required a access token",
-					'ru' => "Требуется ключ доступа"
-				)
-			),
-			'AuthEmailRequired' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "Required a e-mail",
-					'ru' => "Пустой адрес e-mail"
-				)
-			),
-			'AuthEmailNotFound' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "",
-					'ru' => "Адрес e-mail не зарегистрирован"
-				)
-			),
-			'AuthPasswordWrong' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "",
-					'ru' => "Неверный пароль"
-				)
-			),
-			'MethodNotAllowed' => array(
-				'code' => 405,
-				'error_message' => array(
-					'en' => "Method is not supported",
-					'ru' => "Метод не поддерживается"
-				)
-			),
-			'RequestManyCheckpoints' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "Many checkpoints in the request",
-					'ru' => "Запрос содержит лишние контрольные точки"
-				)
-			),
-			'UploadFileTypeWrong' => array(
-				'code' => 400,
-				'error_message' => array(
-					'en' => "Not registered file type",
-					'ru' => "Не зарегистрированный тип файла"
-				)
+				'message' => "Unknown Error"
 			),
 			'UnprocessableEntity' => array(
 				'code' => 422,
-				'error_message' => array(
-					'en' => "Unprocessable Entity",
-					'ru' => "Сущность не поддерживается"
-				)
+				'message' => "Unprocessable Entity"
 			)
 		);
 		
-		public function __construct($type = 'ApiUnknownError', $data=NULL) {
+		public function __construct($type = 'ApiUnknownError', $data = null) {
 			parent::__construct($type);
 			
-			$this->type = $type;
-			$this->data = $data;
+			$this->_type = $type;
+			$this->_data = $data;
 			
-			if(array_key_exists($this->type, $this->_hash))
-				$this->error = $this->_hash[$this->type];
+			if(array_key_exists($this->_type, $this->_hash))
+				$this->error = $this->_hash[$this->_type];
 		}
-		
-		public function code() {
+
+		public function code(): int {
 			return $this->error ? $this->error['code'] : 400;
 		}
-		
-		public function message() {
-			return $this->error ? $this->error['error_message'] : $this->_hash['UnknownError']['error_message'];
+
+		public function data() {
+			return $this->_data;
 		}
 		
-		public function toArray() {
+		public function message(): string {
+			return $this->error ? $this->error['message'] : $this->_hash['UnknownError']['message'];
+		}
+		
+		public function toArray(): array {
 			$result = array();
 			$result['code'] = 400;
 			$result['error_type'] = $this->_type;
 			
-			if($this->_data)
-				$result['error_data'] = $this->_data;
+			if($this->_data) $result['error_data'] = $this->_data;
 			
 			if(array_key_exists($this->_type, $this->_hash))
 				$result = array_merge($result, $this->_hash[$this->_type]);
 			
 			return $result;
 		}
+
+		public function type(): string {
+			return $this->_type;
+		}
 		
-		public function __toString() {
+		public function __toString(): string {
 			return $this->_type;
 		}
 	}
