6b0107f8f93244d9381017387a74c21eea1af1d4
BPanchenko <oberon@list.ru>
2017-01-28 20:57:02 +0300

    refactoring

--- _php/lib/base/Collection.php
+++ _php/lib/base/Collection.php
@@ -1,303 +1,307 @@
 <?php
 namespace base;
 	
-	class Collection extends Component implements \ArrayAccess {
-		public static $classModel = Model;
-		public $models = array();
-		public $length = 0;
-		public $pagination;
-        public $total = 0;
-		protected $_columns = array();
+class Collection extends Component implements \ArrayAccess {
+  public static $classModel = Model;
+  public $models = array();
+  public $length = 0;
+  public $pagination;
+  public $total = 0;
+  protected $_columns = array();
 		
 		
-		function __construct($data = array(), $parent = NULL) {
-			if(is_array($data) && count($data))
-				foreach($data as $item) {
-					$this->add($item);
-				}
-			
-			// init pagination
-			$this->pagination = new \stdClass();
-			$this->pagination->total = 0;
-			$this->pagination->page = 1;
-			$this->pagination->limit = 20;
-			
-			parent::__construct($data, $parent);
-		}
-		
-		
-		/****/
-		public function offsetExists($offset): bool {
-			return isset($this->models[$offset]);
-		}
-		public function offsetGet($offset) {
-			return $this->offsetExists($offset) ? $this->models[$offset] : null;
-		}
+  function __construct($data = array(), $parent = NULL) {
+    if(is_array($data) && count($data)) {
+      foreach($data as $item) {
+        $this->add($item);
+      }
+    }
 
-		public function offsetSet($offset, $model): self {
-			if (is_null($offset)) {
-				$this->models[] = $model;
-			} else {
-				$this->models[$offset] = $model;
-			}
-			return $this;
-		}
+    // init pagination
+    $this->pagination = new \stdClass();
+    $this->pagination->total = 0;
+    $this->pagination->page = 1;
+    $this->pagination->limit = 20;
 
-		public function offsetUnset($offset): bool {
-			$index = NULL;
-			unset($this->models[$index]);
-			
-			return is_null($index) ? false : true;
-		}
+    parent::__construct($data, $parent);
+  }
 		
 		
-		/**
-		 * Добавление элемента в коллекцию
-		 */
-		public function add($data): self {
-			if($data instanceof static::$classModel) {
-				$model = $data;
-				$model->collection = $this;
-			} elseif(is_numeric($data) || is_array($data) || is_object($data)) {
-				$model = new static::$classModel($data, $this);
-			}
-			
-			if(!$model->isValid()) return $this;
-			
-			if($this->get($model->id))
-				$this->get($model->id)->set($model->toArray());
-			else {
-				array_push($this->models, $model);
-				$this->length++;
-				$this->trigger('add', array(
-					'model' => $model
-				));
-			}
-			
-			return $this;
-		}
+  /****/
+  public function offsetExists($offset): bool {
+    return isset($this->models[$offset]);
+  }
+  public function offsetGet($offset) {
+    return $this->offsetExists($offset) ? $this->models[$offset] : null;
+  }
+
+  public function offsetSet($offset, $model): self {
+    if (is_null($offset)) {
+      $this->models[] = $model;
+    } else {
+      $this->models[$offset] = $model;
+    }
+    return $this;
+  }
+
+  public function offsetUnset($offset): bool {
+    $index = NULL;
+    unset($this->models[$index]);
+
+    return is_null($index) ? false : true;
+  }
 		
 		
-		public function at($index) {
-			return $this->models[$index];
-		}
+  /**
+   * Добавление элемента в коллекцию
+   */
+  public function add($data): self {
+    if($data instanceof static::$classModel) {
+      $model = $data;
+      $model->collection = $this;
+    } elseif(is_numeric($data) || is_array($data) || is_object($data)) {
+      $model = new static::$classModel($data, $this);
+    }
+
+    if(!$model->isValid()) return $this;
+
+    if($this->get($model->id))
+      $this->get($model->id)->set($model->toArray());
+    else {
+      array_push($this->models, $model);
+      $this->length++;
+      $this->trigger('add', array(
+          'model' => $model
+      ));
+    }
+
+    return $this;
+  }
 		
 		
-		/**
-		 * Создание модели внутри коллекции.
-		 * Новая модель автоматически добавляется в коллекцию.
-		 */
-		public function create($data){
-			try {
-				$model = new static::$classModel($data, $this);
-				$this->add($model);
-			} catch(ErrorException $e) {
-				if($e->getMessage() == 'WrongModelID') return NULL;
-			}
-			return $model;
-		}
+  public function at($index) {
+    return $this->models[$index];
+  }
 		
 		
-		/**
-		 * Обход элементов коллекции.
-		 * Для каждого элемента вызывается функция, переданая в параметре метода.
-		 * При каждом вызове функции ей будут переданы 3 аргумента: $model, $index, $collection.
-		 */
-		public function each($fn): self {
-			if(!is_callable($fn)) return $this;
-			foreach($this->models as $index=>$model)
-				call_user_func($fn, $model, $index, $this);
-			return $this;
-		}
+  /**
+   * Создание модели внутри коллекции.
+   * Новая модель автоматически добавляется в коллекцию.
+   */
+  public function create($data){
+    try {
+      $model = new static::$classModel($data, $this);
+      $this->add($model);
+    } catch(ErrorException $e) {
+      if($e->getMessage() == 'WrongModelID') return NULL;
+    }
+    return $model;
+  }
 		
 		
-		/**
-		 * Выполняет поиск элемента коллекции,
-		 * подходящего под переданый массив атрибутов.
-		 */
-		public function findWhere($attributes, $value){
-			if(!is_array($attributes) && $value) {
-				$attributes = array(
-					$attributes => $value
-				);
-			}
-			return $this->where($attributes, true);
-		}
+  /**
+   * Обход элементов коллекции.
+   * Для каждого элемента вызывается функция, переданая в параметре метода.
+   * При каждом вызове функции ей будут переданы 3 аргумента: $model, $index, $collection.
+   */
+  public function each($fn): self {
+    if(!is_callable($fn)) return $this;
+    foreach($this->models as $index=>$model) call_user_func($fn, $model, $index, $this);
+    return $this;
+  }
 		
 		
-		/**
-		 * Возвращает модель из коллекции по ее идентификатору.
-		 */
-		public function get($id) {
-			if($id && is_numeric($id))
-				foreach($this->models as $model) {
-					if($model->id == $id) return $model;
-				}
-			
-			return null;
-		}
+  /**
+   * Выполняет поиск элемента коллекции,
+   * подходящего под переданый массив атрибутов.
+   */
+  public function findWhere($attributes, $value){
+    if(!is_array($attributes) && $value) {
+      $attributes = [
+          $attributes => $value
+      ];
+    }
+    return $this->where($attributes, true)[0];
+  }
 		
 		
-		/**
-		 * Поиск модели с минимальным значением атрибута $attr
-		 */
-		public function max($attr) {
-			$result = NULL;
-			
-			for ($i = 0; $i <= $this->length; $i++) {
-				if(!$this[$i]->has($attr))
-					continue;
-				
-				if(is_null($result))
-					$result = $this[$i];
-				elseif($result->get($attr) < $this[$i]->get($attr))
-					$result = $this[$i];
-			}
-			
-			return $result;
-		}
+  /**
+   * Возвращает модель из коллекции по ее идентификатору.
+   */
+  public function get($id) {
+    if($id && is_numeric($id)) {
+      foreach($this->models as $model) {
+        if($model->id == $id) return $model;
+      }
+    }
+
+    return null;
+  }
 		
 		
-		/**
-		 * Поиск модели с минимальным значением атрибута $attr
-		 */
-		public function min($attr) {
-			$result = NULL;
-			
-			for ($i = 0; $i <= $this->length; $i++) {
-				if(!method_exists($this[$i], 'has') || !$this[$i]->has($attr))
-					continue;
-				
-				if(is_null($result))
-					$result = $this[$i];
-				elseif($result->get($attr) > $this[$i]->get($attr))
-					$result = $this[$i];
-			}
-			
-			return $result;
-		}
+  /**
+   * Поиск модели с минимальным значением атрибута $attr
+   */
+  public function max($attr) {
+    $result = NULL;
+
+    for ($i = 0; $i <= $this->length; $i++) {
+      if(!$this[$i]->has($attr)) continue;
+
+      if(is_null($result))
+        $result = $this[$i];
+      elseif($result->get($attr) < $this[$i]->get($attr))
+        $result = $this[$i];
+    }
+
+    return $result;
+  }
 		
 		
-		/**
-		 * Возвращает индекс модели в коллекции.
-		 * Если модели в коллекции нет результатом будет false.
-		 */
-		public function indexOf($searchValue): int {
-			$index = false;
-			$id = $searchValue instanceof Model ? $searchValue->id : (int)$searchValue;
-			
-			foreach($this->models as $key=>$model)
-				if($model->id == $id) {
-					$index = $key;
-					continue;
-				}
-			
-			return $index;
-		}
+  /**
+   * Поиск модели с минимальным значением атрибута $attr
+   */
+  public function min($attr) {
+    $result = NULL;
+
+    for ($i = 0; $i <= $this->length; $i++) {
+      if(!method_exists($this[$i], 'has') || !$this[$i]->has($attr))
+        continue;
+
+      if(is_null($result))
+        $result = $this[$i];
+      elseif($result->get($attr) > $this[$i]->get($attr))
+        $result = $this[$i];
+    }
+
+    return $result;
+  }
 		
 		
-		/****/
-		public function set($data, $end=0): self {
-			if(is_array($data) && count($data))
-				foreach($data as $item) {
-					$this->add($item);
-				}
-			return $this;
-		}
+  /**
+   * Возвращает индекс модели в коллекции.
+   * Если модели в коллекции нет результатом будет false.
+   */
+  public function indexOf($searchValue): int {
+    $index = false;
+    $id = $searchValue instanceof Model ? $searchValue->id : (int)$searchValue;
+
+    foreach($this->models as $key=>$model) {
+      if($model->id == $id) {
+        $index = $key;
+        continue;
+      }
+    }
+
+    return $index;
+  }
 		
 		
-		/**
-		 * Выбирает срез коллекции начиная с идекса $start и заканчивая индексом $end.
-		 */
-		public function slice($start, $end=0) {
-			$offset = $start;
-			 $length = $end ? ($end - $start) : NULL;
-			return  array_slice ($this->models, $offset, $length);
-		}
+  /****/
+  public function set($data, $end=0): self {
+    if(is_array($data) && count($data)) {
+      foreach($data as $item) {
+        $this->add($item);
+      }
+    }
+    return $this;
+  }
 		
 		
-		/**
-		 * Преобразует коллекцию в простой массив,
-		 * каждый элемент которого является ассоциативным массивом атрибутов модели.
-		 */
-		public function toArray(array $options=array()): array {
-			$array = array();
-			if(!$this->length)
-				return $array;
-			
-			if($options['bulk'] == 'ids') {
-				foreach($this->models as $model) {
-					array_push($array,$model->id);
-				}
-			} else {
-				foreach($this->models as $model) {
-					array_push($array,$model->toArray($options));
-				}
-			}
-			return $array;
-		}
-		
-		public function toJSON($options=array()) {
-			return json_encode($this->toArray($options));
-		}
+  /**
+   * Выбирает срез коллекции начиная с идекса $start и заканчивая индексом $end.
+   */
+  public function slice($start, $end=0) {
+    $offset = $start;
+    $length = $end ? ($end - $start) : NULL;
+    return  array_slice ($this->models, $offset, $length);
+  }
 		
 		
-		/**
-		 * Выполняет поиск элементов коллекции,
-		 * подходящих под переданый массив атрибутов.
-		 */
-		public function where(array $attributes, bool $first=false): array{
-			$_result = array();
-			
-			foreach($this->models as $model) {
-				$apt = true;
-				foreach($attributes as $key => $val) {
-					if($model->get($key) != $val) {
-						$apt = false;
-						break;
-					}
-				}
-				if($apt)
-					if($first)
-						return $model;
-					else
-						array_push($_result, $model);
-			}
-			
-			return $_result;
-		}
+  /**
+   * Преобразует коллекцию в простой массив,
+   * каждый элемент которого является ассоциативным массивом атрибутов модели.
+   */
+  public function toArray(array $options=array()): array {
+    $array = array();
+    if(!$this->length) return $array;
+
+    if($options['bulk'] == 'ids') {
+      foreach($this->models as $model) {
+        array_push($array,$model->id);
+      }
+    } else {
+      foreach($this->models as $model) {
+          array_push($array,$model->toArray($options));
+      }
+    }
+    return $array;
+  }
+		
+  public function toJSON($options=array()) {
+    return json_encode($this->toArray($options));
+  }
 		
 		
-		/**
-		 * Вернет массив значений свойства каждого элемента коллекции.
-		 */
-		public function pluck(string $attr): array{
-			$_result = array();
-			
-			foreach($this->models as $model) {
-				if(!$model->has($attr))
-					continue;
-				if(!in_array($model->get($attr), $_result))
-					array_push($_result, $model->get($attr));
-			}
-			
-			return $_result;
-		}
+  /**
+   * Выполняет поиск элементов коллекции,
+   * подходящих под переданый массив атрибутов.
+   */
+  public function where(array $attributes, bool $first=false): array{
+    $_result = array();
+
+    foreach($this->models as $model) {
+      $apt = true;
+      foreach($attributes as $key => $val) {
+        if($model->get($key) != $val) {
+          $apt = false;
+          break;
+        }
+      }
+      if($apt) {
+        if($first) return [$model];
+        else array_push($_result, $model);
+      }
+    }
+
+    return $_result;
+  }
+		
+		
+  /**
+   * Вернет массив значений свойства каждого элемента коллекции.
+   */
+  public function pluck(string $attr): array{
+    $_result = array();
+
+    foreach($this->models as $model) {
+      if(!$model->has($attr))
+        continue;
+      if(!in_array($model->get($attr), $_result))
+        array_push($_result, $model->get($attr));
+    }
+
+    return $_result;
+  }
 
 
-		/* Prepare api response
-		 ========================================================================== */
+  /* API
+   ========================================================================== */
 
-		public function prepareResponse($Response): self {
-			$meta = $Response->get('meta');
-			$meta->length = $this->length;
-			$meta->paging = $this->paging;
-			$meta->total = $this->total;
-			
-			$Response->set('meta', $meta);
-			$Response->set('data', $this->toArray());
+  /**
+   * Обработка ответа на запрос к API
+   */
 
-			return $this;
-		}
-	}
+  public function prepareResponse($Response): self {
+    $meta = $Response->get('meta');
+    $meta->length = $this->length;
+    $meta->paging = $this->paging;
+    $meta->total = $this->total;
+
+    $Response->set('meta', $meta);
+    $Response->set('data', $this->toArray());
+
+    return $this;
+  }
+}
 ?>
