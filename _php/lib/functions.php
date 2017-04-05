4a0beb312eebc08799b0af9e43b85770b1e44b8d
BPanchenko <oberon@list.ru>
2017-04-05 12:41:14 +0300

    add a 'deleteAllCookies' function

--- _php/lib/functions.php
+++ _php/lib/functions.php
@@ -4,65 +4,76 @@ array_walk($_COOKIE, function(&$cookie){ $cookie = trim($cookie, '"'); });
 
 function array_parse_int(array $arr = []) {
   return array_combine(array_keys($arr), array_map(function($val){
-    return is_numeric($val) ? (int)$val : $val;
+    return (is_numeric($val) && $val < PHP_INT_MAX) ? (int)$val : $val;
   }, $arr));
 }
 
+function deleteAllCookies() {
+  $domain = preg_replace('/^(?:.*\.)([\w-]+\.[a-z]{2,6})$/i', '$1', $_SERVER['SERVER_NAME']);
+  $time = time() - 3600;
+  foreach ($_COOKIE as $key=>$value) {
+    setCookie($key, '', $time);
+    setCookie($key, '', $time, '/');
+    setCookie($key, '', $time, '/', $domain);
+  }
+  return true;
+}
+
 function getQueryWithoutParameter($param='') {
-    $query = trim($_SERVER{'QUERY_STRING'}, '?');
-    $params = array();
+  $query = trim($_SERVER{'QUERY_STRING'}, '?');
+  $params = array();
 
-    $temp = explode('&', $query);
-    foreach($temp as $phrase) {
-        if(trim($phrase) && strpos($phrase, $param . '=') === false)
-            array_push($params, $phrase);
-    }
+  $temp = explode('&', $query);
+  foreach($temp as $phrase) {
+    if(trim($phrase) && strpos($phrase, $param . '=') === false) array_push($params, $phrase);
+  }
 
-    return count($params) ? join('&', $params) : null;
+  return count($params) ? join('&', $params) : null;
 }
 
 function translit($string) {
-    $trans = array("а" => "a", "б" => "b", "в" => "v", "г" => "g",
-                    "д" => "d", "е" => "e", "ё" => "e", "ж" => "zh",
-                    "з" => "z", "и" => "i", "й" => "y", "к" => "k",
-                    "л" => "l", "м" => "m", "н" => "n", "о" => "o",
-                    "п" => "p", "р" => "r", "с" => "s", "т" => "t",
-                    "у" => "u", "ф" => "f", "х" => "kh", "ц" => "ts",
-                    "ч" => "ch", "ш" => "sh", "щ" => "shch", "ы" => "y",
-                    "э" => "e", "ю" => "yu", "я" => "ya", "А" => "A",
-                    "Б" => "B", "В" => "V", "Г" => "G", "Д" => "D",
-                    "Е" => "E", "Ё" => "E", "Ж" => "Zh", "З" => "Z",
-                    "И" => "I", "Й" => "Y", "К" => "K", "Л" => "L",
-                    "М" => "M", "Н" => "N", "О" => "O", "П" => "P",
-                    "Р" => "R", "С" => "S", "Т" => "T", "У" => "U",
-                    "Ф" => "F", "Х" => "Kh", "Ц" => "Ts", "Ч" => "Ch",
-                    "Ш" => "Sh", "Щ" => "Shch", "Ы" => "Y", "Э" => "E",
-                    "Ю" => "Yu", "Я" => "Ya", "ь" => "", "Ь" => "",
-                    "ъ" => "", "Ъ" => "");
-    return preg_match("/[а-яА-Я]/",$string) ? strtr($string, $trans) : $string;
+  $trans = array("а" => "a", "б" => "b", "в" => "v", "г" => "g",
+      "д" => "d", "е" => "e", "ё" => "e", "ж" => "zh",
+      "з" => "z", "и" => "i", "й" => "y", "к" => "k",
+      "л" => "l", "м" => "m", "н" => "n", "о" => "o",
+      "п" => "p", "р" => "r", "с" => "s", "т" => "t",
+      "у" => "u", "ф" => "f", "х" => "kh", "ц" => "ts",
+      "ч" => "ch", "ш" => "sh", "щ" => "shch", "ы" => "y",
+      "э" => "e", "ю" => "yu", "я" => "ya", "А" => "A",
+      "Б" => "B", "В" => "V", "Г" => "G", "Д" => "D",
+      "Е" => "E", "Ё" => "E", "Ж" => "Zh", "З" => "Z",
+      "И" => "I", "Й" => "Y", "К" => "K", "Л" => "L",
+      "М" => "M", "Н" => "N", "О" => "O", "П" => "P",
+      "Р" => "R", "С" => "S", "Т" => "T", "У" => "U",
+      "Ф" => "F", "Х" => "Kh", "Ц" => "Ts", "Ч" => "Ch",
+      "Ш" => "Sh", "Щ" => "Shch", "Ы" => "Y", "Э" => "E",
+      "Ю" => "Yu", "Я" => "Ya", "ь" => "", "Ь" => "",
+      "ъ" => "", "Ъ" => "");
+
+  return preg_match("/[а-яА-Я]/",$string) ? strtr($string, $trans) : $string;
 }
 
 function str2array($data) {
-    $result = NULL;
+  $result = NULL;
 
-    if(is_array($data))
-        $result = $data;
-    else if(!empty($data)) {
-        $str = str_replace(array(';','\t','\n'),',',$data);
-        $str = preg_replace('~[^-_a-z0-9,]+~u','',$str);
-        $array = explode(',',$str);
-        if(!is_array($array) && !empty($array)) $array[0] = $array;
-        $result = $array;
-    }
+  if(is_array($data))
+    $result = $data;
+  else if(!empty($data)) {
+    $str = str_replace(array(';','\t','\n'),',',$data);
+    $str = preg_replace('~[^-_a-z0-9,]+~u','',$str);
+    $array = explode(',',$str);
+    if(!is_array($array) && !empty($array)) $array[0] = $array;
+    $result = $array;
+  }
 
-    return $result;
+  return $result;
 }
 
 function camelize($str) {
-    $str = preg_replace('~[^A-Za-z0-9]+~u', ' ', strtolower($str));
-    $str = ucwords($str);
-    $str = str_replace(' ', '',$str);
-    $str = strtolower($str{0}) . substr($str, 1);
-    return $str;
+  $str = preg_replace('~[^A-Za-z0-9]+~u', ' ', strtolower($str));
+  $str = ucwords($str);
+  $str = str_replace(' ', '',$str);
+  $str = strtolower($str{0}) . substr($str, 1);
+  return $str;
 }
 ?>
