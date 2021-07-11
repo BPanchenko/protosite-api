c02bc4df652aeacf265291373e5cb7f547e4fffa
Boris Panchenko <oberon@list.ru>
2021-07-11 15:52:16 +0300

    init constants `API_ROOT` and `STATIC_JSON_DIR`

--- _php/config.php
+++ _php/config.php
@@ -7,9 +7,11 @@ define('DB_ROOT_USER', 'bp_api');
 define('DB_ROOT_PASS', 'nua-B4ZA');
 define('DB_STORAGE_DIR', '/home/bp/api.protosite.rocks/docs/_php/storage');
 
+define('API_ROOT', '/');
 define('LOG_DIR', '/home/bp/api.protosite.rocks/docs/_php/logs');
 define('PHOTOS_UPLOAD_DIR', '/home/bp/api.protosite.rocks/docs/upload/photos/temp');
 define('PHOTOS_PATH', 'http://api.protosite.rocks/upload/photos');
+define('STATIC_JSON_DIR', '/home/bp/api.protosite.rocks/docs/static');
 
 define('FETCH_DEFAULT_COUNT', 200);
 define('FETCH_DEFAULT_OFFSET', 0);
